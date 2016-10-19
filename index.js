532d2255b5d7bb70da681ec175a097595fe0ec0b
djguu <goncalopcorreia@hotmail.com>
2016-10-19 18:16:09 +0100

    gets clientIp now

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var clientIp = socket.request.connection.remoteAddress;
+        clientIp = clientIp.replace(/^.*:/, '');
+  console.log('Utilizador ', clientIp, 'ligou-se');
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', clientIp + ': '+ msg);
   });
 });
+
+/*io.on("connection", function (socket) {
+    var clientIp = socket.request.connection.remoteAddress;
+	clientIp = clientIp.replace(/^.*:/, '');
+    console.log('eventName',{ip : clientIp}); //emit it back to client
+});*/
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
