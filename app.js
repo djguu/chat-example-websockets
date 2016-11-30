ddee94b229a4728817bb352c85209d49c392e311
djguu <goncalopcorreia@hotmail.com>
2016-11-30 15:10:15 +0000

    finished base project

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = new express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var nicknames = [];
 
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-
+app.use(express.static(__dirname + "/public" ))
 
 
 io.on('connection', function(socket){
